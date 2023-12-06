ca5c231c4da2586a77ea583a49c8b4aad3cb05e3
Patrick Bohn <patrick.bohn.pb@gmail.com>
2023-12-06 20:26:46 +0100

    cleaning up part b

--- aoc/src/commands/day6b.rs
+++ aoc/src/commands/day6b.rs
@@ -11,18 +11,8 @@ pub struct Day6b {
 
 impl CommandImpl for Day6b {
     fn main(&self) -> Result<(), DynError> {
-        let races = parse_input(&self.input);
-        for race in &races {
-            race.print_data();
-            race.print_charging_limits();
-            let num_wins = race.num_possible_wins();
-            println!("Num possible wins: {num_wins}");
-        }
-        let num_wins: Vec<usize> = races.iter().map(|x| x.num_possible_wins().to_owned()).collect();
-        for win in &num_wins {
-            println!("{win}");
-        }
-        let result: usize = num_wins.into_iter().product();
+        let race = parse_input(&self.input);
+        let result: usize = race.num_possible_wins();
         println!("Day6a: {result}");
         Ok(())
     }
@@ -38,8 +28,9 @@ impl Boatrace {
     fn find_charging_limits(&self) -> Result<(f64, f64), String> {
         let t_total = self.t_total as f64;
         let distance = self.min_distance as f64;
-        let lower_limit = 0.5_f64 * t_total - (0.25_f64 * t_total.powi(2) as f64 - distance).sqrt();
-        let upper_limit = 0.5_f64 * t_total + (0.25_f64 * t_total.powi(2) as f64 - distance).sqrt();
+        let square_root = (0.25_f64 * t_total.powi(2) as f64 - distance).sqrt();
+        let lower_limit = 0.5_f64 * t_total - square_root;
+        let upper_limit = 0.5_f64 * t_total + square_root;
         Ok((lower_limit, upper_limit))
     }
     fn num_possible_wins(&self) -> usize {
@@ -56,22 +47,9 @@ impl Boatrace {
             Err(_) => 0 as usize,
         }
     }
-    fn print_data(&self) {
-        let t_total = self.t_total;
-        let distance = self.min_distance;
-        println!("t_total: {t_total}, distance: {distance}");
-    }
-
-    fn print_charging_limits(&self) {
-        let t_total = self.t_total as f64;
-        let distance = self.min_distance as f64;
-        let lower_limit = 0.5_f64 * t_total - (0.25_f64 * t_total.powi(2) as f64 - distance).sqrt();
-        let upper_limit = 0.5_f64 * t_total + (0.25_f64 * t_total.powi(2) as f64 - distance).sqrt();
-        println!("lower limit:{lower_limit}, upper limit: {upper_limit}");
-    }
 }
 
-fn parse_input(path: &PathBuf) -> Vec<Boatrace> {
+fn parse_input(path: &PathBuf) -> Boatrace {
     let lines = fs::read_to_string(path).unwrap();
     let mut split_lines = lines.lines();
     let time: usize = split_lines
@@ -85,7 +63,6 @@ fn parse_input(path: &PathBuf) -> Vec<Boatrace> {
         .collect::<String>()
         .parse()
         .expect("Could not convert times to usize");
-    println!("time: {time}");
 
     let distance: usize = split_lines
         .next()
@@ -98,9 +75,7 @@ fn parse_input(path: &PathBuf) -> Vec<Boatrace> {
         .collect::<String>()
         .parse()
         .expect("Could not convert distances to usize");
-    let mut races: Vec<Boatrace> = Vec::new();
-    races.push(Boatrace { t_total: time.to_owned(), min_distance: distance.to_owned() });
-    races
+    Boatrace { t_total: time.to_owned(), min_distance: distance.to_owned() }
 }
 
 /*
